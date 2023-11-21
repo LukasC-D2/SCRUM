f3f6febd828f121413c43b7f952359f5fa7bb8c2
UmmpaLummpa <151522028+UmmpaLummpa@users.noreply.github.com>
2023-11-21 12:45:04 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    idArray = new Array()
+    idArray [1] = "Akėčiomis laukų nepravažinėsi – duonos neturėsi."
+    idArray [2] = "Bijai vilko – neik į mišką."
+    idArray [3] = "Cukrus su druska lygiai balti, tik nelygiai saldūs."
+    idArray [4] = "Čiupinėjasi kaip mergos su bernais."
+    idArray [5] = "Viena boba – vienas turgus, dvi bobos – du turgūs."
+  
+    document.getElementById("select").onclick = myFunction;
+    randomParagraph = Math.floor(Math.random()*5);
+  
+    document.getElementById("result").innerHTML = idArray[randomParagraph + 1];       
+  }
