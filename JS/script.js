992e8b807e323d50b283f42242562b24f0a895ab
LukasC-D2 <151502678+LukasC-D2@users.noreply.github.com>
2023-11-21 13:46:40 +0200

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function changeSaying() {
     idArray = new Array()
     idArray [1] = "Akėčiomis laukų nepravažinėsi – duonos neturėsi."
     idArray [2] = "Bijai vilko – neik į mišką."
@@ -6,7 +6,7 @@ function myFunction() {
     idArray [4] = "Čiupinėjasi kaip mergos su bernais."
     idArray [5] = "Viena boba – vienas turgus, dvi bobos – du turgūs."
   
-    document.getElementById("select").onclick = myFunction;
+    document.getElementById("select").onclick = changeSaying;
     randomParagraph = Math.floor(Math.random()*5);
   
     document.getElementById("result").innerHTML = idArray[randomParagraph + 1];       
